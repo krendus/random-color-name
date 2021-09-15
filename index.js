79a9a3755b0b98b53cd786c6e35a352bcbcd0b58
samurai1979 <xaiyax01@gmail.com>
2021-09-15 13:59:29 +0100

    Added a readme file for package usage

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const randomColor = (array) => {
     let random = Math.floor(Math.random() * array.length)
     return array[random]
 }
-module.exports.random = randomColor(color)
+module.exports.random = () => randomColor(color)
 module.exports.all = color
+console.log(module.exports.random())
